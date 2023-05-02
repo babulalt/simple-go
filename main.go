03f54b1db1e7ff5e9745443ae2501095a1d2abc9
babulalt <babulal.tamang@berrybytes.com>
2023-05-02 12:39:03 +0545

    Add name in hi endpoint

--- main.go
+++ main.go
@@ -20,13 +20,17 @@ func main() {
 		var dur int = 1
 		a := r.URL.Query()
 		ti := a.Get("time")
+		name := a.Get("name")
+		if name == "" {
+			name = "Gophers!!"
+		}
 		if ti != "" {
 			time, _ := strconv.Atoi(ti)
 			dur = int(time)
 		}
 		logrus.Info("Sleep Duration:: ", dur)
 		time.Sleep(time.Duration(dur) * time.Second)
-		fmt.Fprintf(w, "Hi")
+		fmt.Fprintf(w, fmt.Sprintf("Hi %s", name))
 	})
 	logrus.Info("Running on 8000 port...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
